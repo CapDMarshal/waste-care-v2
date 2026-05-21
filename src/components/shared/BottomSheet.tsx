7a884be5305bfa153db9d0003ed54cdc82e72441
CapDMarshal <fardilabintang476@gmail.com>
2026-05-21 07:43:55 +0700

    chore: remove buat campaign button on user side

--- src/components/shared/BottomSheet.tsx
+++ src/components/shared/BottomSheet.tsx
@@ -243,7 +243,7 @@ export const BottomSheet: React.FC<BottomSheetProps> = ({
                     )}
                   
                     {/* Campaign Button - Show different button based on campaign existence */}
-                    {hasCampaign && campaignId ? (
+                    {hasCampaign && campaignId && (
                       <Button
                         fullWidth
                         variant="outline"
@@ -254,15 +254,6 @@ export const BottomSheet: React.FC<BottomSheetProps> = ({
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
                         Buka Campaign
-                      </Button>
-                    ) : (
-                      <Button
-                        fullWidth
-                        variant="outline"
-                        className="border-2 border-[#16a34a] text-[#16a34a] hover:bg-[#15803d] hover:text-white"
-                        onClick={onCreateCampaignClick}
-                      >
-                        Buat Campaign
                       </Button>
                     )}
                   
